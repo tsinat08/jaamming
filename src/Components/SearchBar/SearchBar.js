26e72bb85fa2a453e38244c4120923f05036ff00
tsinat08 <tsinat.weldetnesae@gmail.com>
2019-12-10 05:58:11 -0500

    part 1

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './SearchBar.css'
+
+class SearchBar  extends Component{
+    render() {
+        return (
+            <div className="SearchBar">
+                <input placeholder="Enter A Song, Album, or Artist"/>
+                <button claas="SearchButton">SEARCH</button>
+            </div>
+        )
+    }
+}
